2d0b07df763825ca1ae54b6bb7ed260d8577ef55
Andrew Kane <acekane1@gmail.com>
2014-04-29 21:43:12 -0700

    Cleaned up code

--- lib/ahoy_email.rb
+++ lib/ahoy_email.rb
@@ -5,9 +5,8 @@ require "addressable/uri"
 require "openssl"
 require "ahoy_email/processor"
 require "ahoy_email/interceptor"
+require "ahoy_email/mailer"
 require "ahoy_email/engine"
-
-ActionMailer::Base.register_interceptor AhoyEmail::Interceptor
 
 module AhoyEmail
   mattr_accessor :secret_token, :options
@@ -35,35 +34,5 @@ module AhoyEmail
 
 end
 
-module ActionMailer
-  class Base
-    class_attribute :ahoy_options
-    self.ahoy_options = {}
-
-    class << self
-      def track(options)
-        self.ahoy_options = ahoy_options.merge(options)
-      end
-    end
-
-    def track(options)
-      @ahoy_options = (@ahoy_options || {}).merge(options)
-    end
-
-    def mail_with_ahoy(headers = {}, &block)
-      message = mail_without_ahoy(headers, &block)
-
-      options = AhoyEmail.options.merge(self.class.ahoy_options).merge(@ahoy_options || {})
-      options.each do |k, v|
-        if v.respond_to?(:call)
-          options[k] = v.call(message, self)
-        end
-      end
-      AhoyEmail::Processor.new(message, options).process
-
-      message
-    end
-    alias_method_chain :mail, :ahoy
-
-  end
-end
+ActionMailer::Base.send :include, AhoyEmail::Mailer
+ActionMailer::Base.register_interceptor AhoyEmail::Interceptor
---
+++ lib/ahoy_email/mailer.rb
@@ -0,0 +1,38 @@
+module AhoyEmail
+  module Mailer
+
+    def self.included(base)
+      base.extend ClassMethods
+      base.class_eval do
+        class_attribute :ahoy_options
+        self.ahoy_options = {}
+        alias_method_chain :mail, :ahoy
+      end
+    end
+
+    module ClassMethods
+      def track(options)
+        self.ahoy_options = ahoy_options.merge(options)
+      end
+    end
+
+    def track(options)
+      @ahoy_options = (@ahoy_options || {}).merge(options)
+    end
+
+    def mail_with_ahoy(headers = {}, &block)
+      message = mail_without_ahoy(headers, &block)
+
+      options = AhoyEmail.options.merge(self.class.ahoy_options).merge(@ahoy_options || {})
+      options.each do |k, v|
+        if v.respond_to?(:call)
+          options[k] = v.call(message, self)
+        end
+      end
+      AhoyEmail::Processor.new(message, options).process
+
+      message
+    end
+
+  end
+end
