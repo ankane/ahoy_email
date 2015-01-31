89bd597a87de2ac6885c94131ae19109a378f275
Andrew Kane <acekane1@gmail.com>
2015-01-31 13:27:47 -0800

    Only set secret token if not already set for #24

--- lib/ahoy_email/engine.rb
+++ lib/ahoy_email/engine.rb
@@ -3,7 +3,7 @@ module AhoyEmail
 
     initializer "ahoy_email" do |app|
       secrets = app.respond_to?(:secrets) ? app.secrets : app.config
-      AhoyEmail.secret_token = secrets.respond_to?(:secret_key_base) ? secrets.secret_key_base : secrets.secret_token
+      AhoyEmail.secret_token ||= secrets.respond_to?(:secret_key_base) ? secrets.secret_key_base : secrets.secret_token
     end
 
   end
