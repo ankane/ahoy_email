58734af800068fa8dafb378c0e149131883e8a2a
Ty Rauber <tyrauber@mac.com>
2016-04-22 10:19:14 -0500

    track unsubscribe_links: true Provide the ability to add utm tracking on unsubscribe links as they are ignored by default.  Update the Readme to reflect this behavior.

--- lib/ahoy_email/processor.rb
+++ lib/ahoy_email/processor.rb
@@ -139,7 +139,7 @@ module AhoyEmail
         # remove it
         link.remove_attribute(attribute)
         true
-      elsif link["href"].to_s =~ /unsubscribe/i
+      elsif link["href"].to_s =~ /unsubscribe/i && !options[:unsubscribe_links]
         # try to avoid unsubscribe links
         true
       else
