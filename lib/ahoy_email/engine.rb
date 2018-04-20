63cc071d42b501d985048d8ab9a01585312fcd4c
Andrew <acekane1@gmail.com>
2018-04-19 19:20:00 -0700

    Silence deprecation warning

--- lib/ahoy_email/engine.rb
+++ lib/ahoy_email/engine.rb
@@ -3,8 +3,12 @@ require "rails/engine"
 module AhoyEmail
   class Engine < ::Rails::Engine
     initializer "ahoy_email" do |app|
-      secrets = app.respond_to?(:secrets) ? app.secrets : app.config
-      AhoyEmail.secret_token ||= secrets.respond_to?(:secret_key_base) ? secrets.secret_key_base : secrets.secret_token
+      # default to secrets to keep backward compatible
+      ActiveSupport::Deprecation.silence do
+        secrets = app.respond_to?(:secrets) ? app.secrets : app.config
+        AhoyEmail.secret_token ||= secrets.respond_to?(:secret_key_base) ? secrets.secret_key_base : secrets.secret_token
+      end
+
       AhoyEmail.belongs_to = {optional: true} if Rails::VERSION::MAJOR >= 5
     end
   end
