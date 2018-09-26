a37e9d4090bcac37638735b3eec889517c257a27
Andrew <acekane1@gmail.com>
2018-09-25 22:09:53 -0700

    Better callback name [skip ci]

--- lib/ahoy_email/mailer.rb
+++ lib/ahoy_email/mailer.rb
@@ -6,7 +6,7 @@ module AhoyEmail
       attr_accessor :ahoy_options
       class_attribute :ahoy_options
       self.ahoy_options = {}
-      after_action :ahoy_track_message
+      after_action :track_ahoy_message
     end
 
     class_methods do
@@ -19,7 +19,7 @@ module AhoyEmail
       self.ahoy_options = (ahoy_options || {}).merge(message: true).merge(options)
     end
 
-    def ahoy_track_message
+    def track_ahoy_message
       AhoyEmail::Processor.new(message, self).process
     end
   end
