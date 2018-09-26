8a2c669a5ca89691aef7a9b2df2eda4837c43ee9
Andrew <acekane1@gmail.com>
2018-09-25 22:07:41 -0700

    Cleaned up test helper [skip ci]

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,8 +4,6 @@ require "minitest/autorun"
 require "minitest/pride"
 require "combustion"
 
-Minitest::Test = Minitest::Unit::TestCase unless defined?(Minitest::Test)
-
 Combustion.path = "test/internal"
 Combustion.initialize! :all do
   if config.active_record.sqlite3.respond_to?(:represent_boolean_as_integer)
