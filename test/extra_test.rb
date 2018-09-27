8a6576f28bd3acac356749cb0e2e09f90f8c72d9
Andrew <acekane1@gmail.com>
2018-09-27 03:24:35 -0700

    Updated tests

--- test/extra_test.rb
+++ test/extra_test.rb
@@ -1,8 +1,13 @@
 require_relative "test_helper"
 
 class ExtraTest < Minitest::Test
-  def test_string
+  def test_default
     ExtraMailer.welcome.deliver_now
+    assert_nil ahoy_message.coupon_id
+  end
+
+  def test_static
+    ExtraMailer.basic.deliver_now
     assert_equal 1, ahoy_message.coupon_id
   end
 
--- test/internal/app/mailers/extra_mailer.rb
+++ test/internal/app/mailers/extra_mailer.rb
@@ -1,8 +1,12 @@
 class ExtraMailer < ApplicationMailer
-  track extra: {coupon_id: 1}, only: [:welcome]
+  track extra: {coupon_id: 1}, only: [:basic]
   track extra: -> { {coupon_id: params[:coupon_id]} }, only: [:other]
 
   def welcome
+    mail
+  end
+
+  def basic
     mail
   end
 
