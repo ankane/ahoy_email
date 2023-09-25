77da0ee8386fd836c7a57c2c86a9558647b5f969
Andrew Kane <andrew@ankane.org>
2023-09-24 18:48:39 -0700

    Fixed issue with Mailkick and SECRET_KEY_BASE_DUMMY with Rails 7.1

--- lib/ahoy_email/engine.rb
+++ lib/ahoy_email/engine.rb
@@ -4,6 +4,14 @@ module AhoyEmail
   class Engine < ::Rails::Engine
     initializer "ahoy_email" do |app|
       AhoyEmail.secret_token ||= begin
+        # Fix for issue with Mailkick and SECRET_KEY_BASE_DUMMY with Rails 7.1
+        # https://github.com/ankane/mailkick/pull/74
+        if Rails::VERSION::STRING.to_f >= 7.1 && ENV["SECRET_KEY_BASE_DUMMY"]
+          # TODO use for token in 3.0
+          app.key_generator.generate_key("ahoy_email")
+        end
+
+        # TODO remove in 3.0
         creds =
           if app.respond_to?(:credentials) && app.credentials.secret_key_base
             app.credentials
