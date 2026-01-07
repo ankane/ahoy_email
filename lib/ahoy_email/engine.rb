a34e23e5b869711c76aafde8ef856d6901fed8e4
Filip Zachar <tulak45@gmail.com>
2026-01-08 00:32:38 +0100

    Fixes initializer to run after Rails configuration is finalized

--- lib/ahoy_email/engine.rb
+++ lib/ahoy_email/engine.rb
@@ -2,8 +2,10 @@ require "rails/engine"
 
 module AhoyEmail
   class Engine < ::Rails::Engine
-    initializer "ahoy_email" do |app|
-      AhoyEmail.secret_token ||= app.key_generator.generate_key("ahoy_email")
+    initializer "ahoy_email", after: :load_config_initializers do |app|
+      app.config.after_initialize do
+        AhoyEmail.secret_token ||= app.key_generator.generate_key("ahoy_email")
+      end
     end
   end
 end
